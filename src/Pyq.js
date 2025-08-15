5e0c887e46482b7394160dc223b31f28e30beb91
sardanahardik0-cpu <sardanahardik0@gmail.com>
2025-08-15 19:09:40 +0530

    Create Pyq.js

---
+++ src/Pyq.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Pyq() {
+  return (
+    <div>
+      <h2>Previous Year Questions</h2>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
+
+export default Pyq;
