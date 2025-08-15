bde878b9ccd1420df36f44075c7e9962139ac4ef
sardanahardik0-cpu <sardanahardik0@gmail.com>
2025-08-15 18:52:47 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css"; // Keep your existing styles
+
+function App() {
+  return (
+    <div className="App">
+      <h1>JEE Crackr</h1>
+      <p>Welcome to your JEE preparation app!</p>
+    </div>
+  );
+}
+
+export default App;
